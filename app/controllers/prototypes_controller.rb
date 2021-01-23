b13b38b28700fd4905817df0b7eb73976522236e
yano-kentaro <ynkn0829@gmail.com>
2021-01-23 17:51:06 +0900

    ログイン中のユーザーが新規投稿出来る

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @prototypes = Prototype.all
