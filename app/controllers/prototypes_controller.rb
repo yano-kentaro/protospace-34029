d7a08e6f83d03eaf2c18375113cad3535d33347f
yano-kentaro <ynkn0829@gmail.com>
2021-01-23 17:37:54 +0900

    ログアウト状態でもトップページに遷移出来るよう変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:new, :edit, :destroy]
 
   def index
     @prototypes = Prototype.all
